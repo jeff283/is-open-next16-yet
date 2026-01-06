e553af02255083f6e93cac7eaafc2a23ce92def6
Jeff Njoroge <60646637+jeff283@users.noreply.github.com>
2026-01-06 23:25:06 +0300

    Initial prototype

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,44 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { z } from 'zod'
 
-export const Route = createFileRoute('/')({ component: App })
+const packageJsonSchema = z.object({
+  dependencies: z.object({
+    next: z.string(),
+  }),
+})
+
+const packageJsonUrl =
+  'https://raw.githubusercontent.com/opennextjs/opennextjs-cloudflare/refs/heads/main/create-cloudflare/next/package.json'
+
+// Next js 16 and above
+const targetVersion = 16
+
+const getOpenNextVersion = async () => {
+  const response = await fetch(packageJsonUrl)
+  const data = await response.json()
+  const parsed = packageJsonSchema.parse(data)
+  const version = parsed.dependencies.next
+  const versionNumber = parseInt(version.split('.')[0], 10)
+  const responseData = {
+    isOpenNext16Yet: versionNumber >= targetVersion,
+    versionNumber,
+    version,
+  }
+  return responseData
+}
+export const Route = createFileRoute('/')({
+  loader: () => getOpenNextVersion(),
+  component: App,
+})
 
 function App() {
+  const { isOpenNext16Yet, versionNumber, version } = Route.useLoaderData()
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Open Next 16 Yet? {isOpenNext16Yet ? 'Yes' : 'No'}</h1>
+      <p>Version: {versionNumber}</p>
+      <p>Version: {version}</p>
+      <p>Target Version: {targetVersion}</p>
     </div>
   )
 }
