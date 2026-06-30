ed39494637e4c20ce88741912db65e4d2fdf7f90
jeff283 <60646637+jeff283@users.noreply.github.com>
2026-06-30 16:26:42 +0300

    Add NPM registry URLs to constants.ts for improved package management

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,6 +2,12 @@
 export const PACKAGE_JSON_URL =
   'https://raw.githubusercontent.com/opennextjs/opennextjs-cloudflare/refs/heads/main/create-cloudflare/next/package.json'
 
+// NPM REGISTRY URL
+export const NPM_REGISTRY_URL = 'https://registry.npmjs.org'
+
+// NEXT LATEST REGISTRY URL
+export const NEXT_LATEST_REGISTRY_URL = 'https://registry.npmjs.org/next/latest'
+
 export const GITHUB_ISSUE_URL =
   'https://api.github.com/repos/opennextjs/opennextjs-cloudflare/issues/972'
 
